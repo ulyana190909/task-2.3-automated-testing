a7c363ab26af09d69921b0338897b40e859a3de7
Uliana Utina <ulyana190909@yandex.ru>
2020-10-27 19:21:55 +0300

    упавший тест

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,12 +1,14 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
     @Test
     void postmanEchoTest() {
         given()
                 .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
@@ -14,7 +16,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("some value"))
         ;
     }
 }
